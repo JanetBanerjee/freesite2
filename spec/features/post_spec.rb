002a04c939b08ac094c61f99d8634ac6bc481d6f
Janet Banerjee <banerjee.janet@gmail.com>
2017-09-28 17:54:30 -0400

    creating new posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,11 +6,26 @@ describe 'navigate' do
       visit posts_path
       expect(page.status_code).to eq(200)
     end
+
+    it 'has a title of posts' do
+      visit posts_path
+      expect(page).to have_content(/Posts/)
+    end
   end
 
-  it 'has a title of posts' do
-    visit posts_path
-    expect(page).to have_content(/Posts/)
+  describe 'creation' do
+    it 'has a new form that can be reached' do
+      visit new_post_path
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'can be created from new form page' do
+      visit new_post_path
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[item]', with: "Some item"
+      click_on "Save"
+      expect(page).to have_content("Some item")
+    end
   end
 
 end
